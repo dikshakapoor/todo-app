6d44b6ae182ca12dbdab1e3d59624a1f72b3a39c
dikshakapoor <52195441+dikshakapoor@users.noreply.github.com>
2019-10-02 23:32:44 +0530

    deleting item for specific id is possible now

--- todo.js
+++ todo.js
@@ -1,3 +1,84 @@
 var arr = [];
+document.getElementById("btn").addEventListener ('click', function ()
+{
+    var value = document.getElementById("inputfield").value;
+     if (value) {
+     var task = createNewObject(value);
+     console.log ("this is task", task);
+     objectArray(task);
+     chnageInDOM(task);
+     }
+});
 
-arr[i]
+function createNewObject(text){
+//innerHtml is not efficent, it takes a lot of memory
+    
+   var  task = {
+        text: text,
+        id : new Date().getUTCMilliseconds(),
+        Completed : false,
+       
+    };
+
+    console.log(`this is inside create object`,task);
+    
+    return task;
+    }
+    
+    function objectArray(task){
+        
+        arr.push(task);
+        console.log(arr);
+         //called here because now the taskObject is not empty
+    }
+ //changing DOM and inserting new task on click at add button
+    function chnageInDOM (task){
+    
+        var html,newhtml;
+      html = ' <li  id = "%id%"> %Value% <div class = "icons"><div class = "complete_button"><img src = "_ionicons_svg_md-checkmark-circle.svg" width = "20px" heigth = "20px"><div class = "edit_button"><img src = "_ionicons_svg_md-create.svg" width = "20px" heigth = "20px"><div class = "delete_button"><img src = "_ionicons_svg_md-trash.svg"  width = "20px" height = "20px"></div></div></div> </div> </li>'
+     
+     newhtml = html.replace('%Value%',task.text);
+     
+     newhtml = newhtml.replace('%id%',task.id);// over ridding newhtml
+     
+     
+     document.querySelector("#taskList_wrapper").insertAdjacentHTML('beforeend',newhtml);
+    }
+ //deleting item form UI and datastructure  
+    document.querySelector("#taskList_wrapper").addEventListener('click', deleteSelectedItem );
+
+
+function deleteSelectedItem (event) // event is need to know target element
+    {
+      itemID = parseInt(event.target.parentNode.parentNode.parentNode.parentNode.parentNode.id); // to get the unique id of object to be removed form list
+      
+      ;
+
+      if (itemID){
+          //creating array of Id to find the indexof id to be delted
+          ids = arr.map(function(task){
+           return task.id;
+        
+          });
+
+          index = ids.indexOf(itemID);
+          
+          if (index !== -1){
+            deletingIdFromUI(itemID);
+              arr.splice(index,1);
+             
+          }
+      }
+    }
+  
+function deletingIdFromUI(selectorID) {
+   console.log( document.getElementById(selectorID).parentNode);
+   var element = document.getElementById(selectorID);
+   element.parentNode.removeChild(element);
+}
+
+// editing the selected task from the todo list
+
+document.querySelector("#taskList_wrapper").addEventListener('click', compeleteSelectedItem );
+
+function compeleteSelectedItem ()
