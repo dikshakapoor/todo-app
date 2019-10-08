25bfa87e66fbf1e23f07061cdd20efb32fd9e873
dikshakapoor <52195441+dikshakapoor@users.noreply.github.com>
2019-10-08 18:11:48 +0530

    with better structure and css

--- todo_modulePattern.js
+++ todo_modulePattern.js
@@ -2,12 +2,12 @@
 
 // module for data storage 
 var dataController = (function(){
-   var TaskObject = function(task){
-       this.text = task;
-       this.id = Date.now();
-       this.completed = false;
-       this.deleted = false;
-       this.edit = false;
+        var TaskObject = function(task){
+            this.text = task;
+            this.id = Date.now();
+            this.completed = false;
+            this.deleted = false;
+            this.edit = false;
    };
 
    
@@ -18,27 +18,20 @@ var dataController = (function(){
 return  {
     
     addTask : function(task){
-        let newTask; 
-        newTask = new TaskObject(task);
-        taskMap.set(newTask.id, newTask );
+         let newTask; 
+         newTask = new TaskObject(task);
+         taskMap.set(newTask.id, newTask );
 
-        console.log(taskMap);
-        return newTask;
+         console.log(taskMap);
+         return newTask;
         
     },
     testing : function (){
-        console.log (taskMap);
+         console.log (taskMap);
     },
-    // deleteTask : function (id){
-    //         taskMap.remove(id);
-    //        return true; 
-    // },
+   
     compeleteSelectedItem: function(itemID){
        
-        // if (itemID !== -1 && itemID !== undefined)
-
-    // {  let taskComplete = document.getElementById(itemID);
-    //     taskComplete.classList.add("checked");}
          
          taskMap.get(itemID).completed = true;
          console.log(taskMap);
@@ -46,28 +39,27 @@ return  {
     },
     deleteSelectedItem: function(itemID ){
        
-            taskMap.get(itemID).deleted = true;
-            console.log("the deleteselectd works");
-            return taskMap;   
+         taskMap.get(itemID).deleted = true;
+         console.log("the deleteselectd works");
+         return taskMap;   
     
     },
     editSelectedItem: function(itemID){
-        taskMap.get(itemID).edit = true;
-        return taskMap;
+         taskMap.get(itemID).edit = true;
+         return taskMap;
     
     }, 
     completeAll: function() {
-        taskMap.forEach(function(task){
-        task.completed = true;
-        console.log("it works");
-        console.log("this is the task Map ",taskMap);  
+         taskMap.forEach(function(task){
+         task.completed = true;
+          
     })
     return taskMap;},
 
     removeAll: function(){
-    taskMap.forEach(function(task){
-    task.deleted = true;
-    console.log(taskMap);
+         taskMap.forEach(function(task){
+         task.deleted = true;
+         
     
     })
     return taskMap;},
@@ -81,51 +73,45 @@ return  {
 var UIController  = (function(){
 
     DOMStrings = {
-        inputValue: "#inputfield",
-        inputButton: "#btn",
-        inputContainer:"#taskList_wrapper",
+         inputValue: "#inputfield",
+         inputButton: "#btn",
+         inputContainer:"#taskList_wrapper",
     }
 
  return { getInput: function (){
-        return document.querySelector(DOMStrings.inputValue).value;
+         return document.querySelector(DOMStrings.inputValue).value;
             },
-        getDOMString: function (){
-            return DOMStrings ;
+         getDOMString: function (){
+             return DOMStrings ;
         },
         addListItem: function(task) {
-        var html,newhtml,element;
-        // create html string with placeholder tag;
+            var html,newhtml,element;
+                // create html string with placeholder tag;
 
-        html =  '<div class = "card"><div class = "task" id = %id%><b>%Value%</b>'+
-        '</div><div class = "icon"><button class = "complete"><img src =_ionicons_svg_md-checkmark-circle.svg width = "20px" heigth = "20px">'+
-        '</button><button class = "remove"><img src = "_ionicons_svg_md-trash.svg"  width = "20px" height = "20px"></button><button class = "edit">'+
-        '<img src = "_ionicons_svg_md-create.svg" width = "20px" heigth = "20px"></button></button></div></div>'
-        
-        //repclace palceholder with html text;
-        newhtml = html.replace('%Value%',task.text);
-        
-        // place the it into DOM
-        newhtml = newhtml.replace('%id%',task.id);
-        element = DOMStrings.inputContainer;
-        document.querySelector(element).insertAdjacentHTML('beforeend',newhtml);
+            html =  '<div class = "card"><div class = "task" id = %id%><b>%Value%</b>'+
+            '</div><div class = "icon"><button class = "complete"><img src =_ionicons_svg_md-checkmark-circle.svg width = "20px" heigth = "20px">'+
+            '</button><button class = "remove"><img src = "_ionicons_svg_md-trash.svg"  width = "20px" height = "20px"></button><button class = "edit">'+
+            '<img src = "_ionicons_svg_md-create.svg" width = "20px" heigth = "20px"></button></button></div></div>'
+                
+            //repclace palceholder with html text;
+            newhtml = html.replace('%Value%',task.text);
+                
+            // place the it into DOM
+            newhtml = newhtml.replace('%id%',task.id);
+            element = DOMStrings.inputContainer;
+            document.querySelector(element).insertAdjacentHTML('beforeend',newhtml);
 
         },
 
         clearFields: function (){
-        document.getElementById("inputfield").value = "";
-        console.log("it works");
+            document.getElementById("inputfield").value = "";
+            console.log("it works");
         },
-        // removeTask: function(taskID){
-
-        // let element, elementParent;
-        // elementParent = document.getElementById ("taskList_wrapper");
-        // element = document.getElementById(taskID);
-        // elementParent.removeChild(element.parentNode);
-        // return 
-        // },
+       
         display: function(taskMap){
-            console.log(taskMap);
+             console.log(taskMap);
              taskMap.forEach(function(task){
+                  
                     if (task.completed == true) {
                         document.getElementById(task.id).classList.add("checked");
                     }
@@ -137,7 +123,7 @@ var UIController  = (function(){
                     element1.removeChild(element.parentNode);
                     taskMap.delete(task.id);
                     console.log(taskMap);
-                }
+                    }
                    if (task.edit == true){
                     
                     let element = document.getElementById(task.id);
@@ -171,81 +157,81 @@ var UIController  = (function(){
 
 //  global app controller
 var controller = (function(dataCtr, UICtr){    // changing the name of data and UI controller
-var input, addItem;
+     var input, addItem;
 
-var DOM = UICtr.getDOMString();
-console.log(DOM);
-var ctrlAddItem = function (){
-    // get input data
-     input = UICtr.getInput();
+     var DOM = UICtr.getDOMString();
+     console.log(DOM);
+     var ctrlAddItem = function (){
+        // get input data
+         input = UICtr.getInput();
 
-   if ((input !== "") &&  (input !== undefined)) // checking input is present
-   {
-// add item to datacontroller
-    addItem = dataCtr.addTask(input);
+         if ((input !== "") &&  (input !== undefined)) // checking input is present
+         {
+         // add item to datacontroller
+         addItem = dataCtr.addTask(input);
 
-//add item to UIController
-    UICtr.addListItem(addItem);
+         //add item to UIController
+         UICtr.addListItem(addItem);
 
-// clearing input field
-    UICtr.clearFields();
+         // clearing input field
+         UICtr.clearFields();
 }
 }
-var setEventListeners = function () {
-document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+     var setEventListeners = function () {
+         document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
-document.addEventListener('keypress', function(event){
-    if (event.keyCode === 13 || event.which === 13){
-       ctrlAddItem ();
-    }
-});
-}
-document.querySelector("#taskList_wrapper").addEventListener('click', typeOfEvent  );
+         document.addEventListener('keypress', function(event){
+         if (event.keyCode === 13 || event.which === 13){
+         ctrlAddItem ();
+            }
+        });
+        }
+    document.querySelector("#taskList_wrapper").addEventListener('click', typeOfEvent  );
 
 
-function typeOfEvent (event){
-    console.log("working");
-    var eventType = event.target.parentNode.className;
-    console.log("the event is ",eventType);
-    if (eventType == "complete" || eventType == "remove"||eventType == "edit")
-    {  itemID = parseInt(event.target.parentNode.parentNode.previousSibling.id);
-        console.log(itemID);
-        console.log("this is item id ", itemID);
-        if (eventType == "complete") 
-        {  let taskEdited = dataCtr.compeleteSelectedItem(itemID );
-        UICtr.display(taskEdited);}
-        else if (eventType == "remove") 
-        {  console.log("type of event works for remove");
-            let taskEdited = dataCtr.deleteSelectedItem(itemID);
-            UICtr.display(taskEdited);}
-        else if(eventType == "edit") 
-        {   let taskEdited = dataCtr.editSelectedItem(itemID );
-            UICtr.display(taskEdited);}
+     function typeOfEvent (event){
+         console.log("working");
+         var eventType = event.target.parentNode.className;
+         console.log("the event is ",eventType);
+         if (eventType == "complete" || eventType == "remove"||eventType == "edit")
+            {  itemID = parseInt(event.target.parentNode.parentNode.previousSibling.id);
+                console.log(itemID);
+                console.log("this is item id ", itemID);
+                if (eventType == "complete") 
+                {  let taskEdited = dataCtr.compeleteSelectedItem(itemID );
+                UICtr.display(taskEdited);}
+                else if (eventType == "remove") 
+                {  console.log("type of event works for remove");
+                    let taskEdited = dataCtr.deleteSelectedItem(itemID);
+                    UICtr.display(taskEdited);}
+                else if(eventType == "edit") 
+                {   let taskEdited = dataCtr.editSelectedItem(itemID );
+                    UICtr.display(taskEdited);}
 
-    }
-}
+            }
+     }    
 
-document.getElementById("markAllComplete").addEventListener ('click', function ()
+    document.getElementById("markAllComplete").addEventListener ('click', function ()
     { 
-    let taskCompelete = dataCtr.completeAll();
-    console.log(taskCompelete);
-    UICtr.display(taskCompelete);
+         let taskCompelete = dataCtr.completeAll();
+         console.log(taskCompelete);
+         UICtr.display(taskCompelete);
     
- });
+    });
 
 // delete all tast for UI
-document.getElementById("deleteAll").addEventListener ('click', function ()  
+    document.getElementById("deleteAll").addEventListener ('click', function ()  
     {  
-        let  taskRemoved = dataCtr.removeAll();
-        console.log(taskRemoved);
-        UICtr.display(taskRemoved);
+         let  taskRemoved = dataCtr.removeAll();
+         console.log(taskRemoved);
+         UICtr.display(taskRemoved);
          
     });
 
-return {
-    init : function(){
-     console.log ("Application has Started ");
-     setEventListeners();
+    return {
+         init : function(){
+         console.log ("Application has Started ");
+         setEventListeners();
     }
 }
 })(dataController, UIController);
