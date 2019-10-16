5eb47f4271135f6d8e9a73e442d1c275795e787c
dikshakapoor <52195441+dikshakapoor@users.noreply.github.com>
2019-10-16 18:22:47 +0530

    edit event works now
    the edit event works now with modular approach

--- todo_modulePattern.js
+++ todo_modulePattern.js
@@ -26,6 +26,7 @@ let dataController = (function() {
     },
     updateTask: function(updatedTask, id) {
       taskMap.get(id).text = updatedTask;
+      console.log("the updated task", updatedTask);
     },
     markListCompleted: function() {
       taskMap.forEach(function(task) {
@@ -41,17 +42,6 @@ let dataController = (function() {
 })();
 
 let UIController = (function() {
-  function editTaskOnEnter(id) {
-    return function(ev) {
-      if (ev.keyCode !== 13) return null;
-      element = document.getElementById(id);
-      element.classList.add("edit");
-      let task = element.textContent;
-      task.text = task;
-      element.setAttribute("contenteditable", false);
-      controller.updateEditedTask(task, id);
-    };
-  }
   return {
     getInput: function() {
       return document.querySelector(".inputfield").value;
@@ -61,9 +51,8 @@ let UIController = (function() {
       element.classList.remove("edit");
       element.setAttribute("contenteditable", true);
       element.focus();
-      element.addEventListener("keyup", editTaskOnEnter(task.id));
     },
-    renderItems: function(taskList) {
+    renderItems: function(taskList, callback) {
       let element = document.getElementsByClassName("taskList_wrapper");
       element[0].innerHTML = "";
       taskList.forEach(function(task) {
@@ -73,18 +62,28 @@ let UIController = (function() {
           '</div><div class = "icon"><button class = "completed"><img src =_ionicons_svg_md-checkmark-circle.svg width = "20px" heigth = "20px">' +
           '</button><button class = "removed"><img src = "_ionicons_svg_md-trash.svg"  width = "20px" height = "20px"></button><button class = "edited">' +
           '<img src = "_ionicons_svg_md-create.svg" width = "20px" heigth = "20px"></button></button></div></div>';
+
         html = html.replace("%text%", task.text);
         html = html.replace("%id%", task.id);
         element[0].insertAdjacentHTML("beforeend", html);
         switch (task.status) {
           case "edited":
-            UIController.editTask(task);
+            // UIController.editTask(task);
+            {
+              let element = document.getElementById(task.id);
+              element.classList.remove("edit");
+              element.setAttribute("contenteditable", true);
+
+              element.focus();
+            }
             break;
           case "completed":
             document.getElementById(task.id).classList.add("checked");
             break;
         }
       });
+      callback && callback();
+      return taskList;
     },
     clearInputField: function() {
       document.getElementsByClassName("inputfield")[0].value = "";
@@ -94,7 +93,7 @@ let UIController = (function() {
 
 let controller = (function(dataCtr, UICtr) {
   let task, addItem;
-  let ctrlAddItem = function() {
+  let fetchNewTask = function() {
     task = UICtr.getInput().replace(/^\s+|\s+$/gm, "");
     if (task !== "" && task !== undefined) {
       // add item to datacontroller
@@ -105,25 +104,55 @@ let controller = (function(dataCtr, UICtr) {
       UICtr.clearInputField();
     }
   };
-  let setEventListeners = function() {
+  let setEventListeners = function(updateItemId) {
     document
       .getElementsByClassName("addTaskButton")[0]
-      .addEventListener("click", ctrlAddItem);
+      .addEventListener("click", fetchNewTask);
     document
       .getElementsByClassName("taskList_wrapper")[0]
       .addEventListener("click", getTypeOfEvent);
-    document.addEventListener("keypress", addTaskOnEnter(event));
+    document
+      .getElementsByClassName("inputfield")[0]
+      .addEventListener("keypress", addTaskOnEnter);
     document
       .getElementsByClassName("markAllComplete")[0]
       .addEventListener("click", markAllComplete);
     document
       .getElementsByClassName("deleteAll")[0]
       .addEventListener("click", deleteAll);
+    if (updateItemId) {
+      document
+        .getElementById(updateItemId)
+        .addEventListener("keyup", editTaskOnEnter(updateItemId));
+    }
   };
+  function editTaskOnEnter(id) {
+    console.log("enter was pressed ");
+
+    return function(ev) {
+      console.log("the target is ", ev.target);
+      console.log(
+        "the event target is ",
+        event.target.parentNode.parentNode.previousSibling.id
+      );
+      console.log(ev.keyCode);
+      console.log("enter was pressed ");
+      if (ev.keyCode !== 13 || ev.which !== 13) return null;
+
+      console.log("enter was pressed ");
+      element = document.getElementById(id);
+      element.classList.add("edit");
+      let task = element.textContent;
+      console.log("the text entered", task);
+      element.setAttribute("contenteditable", false);
+
+      updateEditedTask(task, id);
+    };
+  }
 
   function addTaskOnEnter(event) {
     if (event != undefined && (event.keyCode === 13 || event.which === 13)) {
-      ctrlAddItem();
+      fetchNewTask();
     }
   }
 
@@ -134,7 +163,13 @@ let controller = (function(dataCtr, UICtr) {
       fetchEventType == "removed" ||
       fetchEventType == "edited"
     ) {
-      itemId = parseInt(event.target.parentNode.parentNode.previousSibling.id);
+      console.log(
+        parseInt(event.target.parentNode.parentNode.previousSibling.id)
+      );
+      itemId = parseInt(
+        parseInt(event.target.parentNode.parentNode.previousSibling.id)
+      );
+
       switch (fetchEventType) {
         case "completed": {
           let taskList = dataCtr.setStatusCompeleted(itemId);
@@ -148,11 +183,18 @@ let controller = (function(dataCtr, UICtr) {
         }
         case "edited": {
           let taskList = dataCtr.setStatusEdited(itemId);
-          UICtr.renderItems(taskList);
+          UICtr.renderItems(taskList, () =>
+            setEventListeners(itemId.toString())
+          );
           break;
         }
       }
     }
+  }
+
+  function updateEditedTask(task, Id) {
+    Id = Number(Id);
+    dataCtr.updateTask(task, Id);
   }
 
   function markAllComplete() {
@@ -167,9 +209,6 @@ let controller = (function(dataCtr, UICtr) {
   return {
     init: function() {
       setEventListeners();
-    },
-    updateEditedTask: function(task, id) {
-      dataCtr.updateTask(task, id);
     }
   };
 })(dataController, UIController);
